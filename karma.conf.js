32cd70af460fd776a69a3a5ae05b380f5696991d
Matthew Hill <matthew.jh@outlook.com>
2015-09-27 21:37:30 +0100

    chore(karma.conf): adds test infrastructurer

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
@@ -27,8 +27,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'test/**/*.js': ['browserify']
     },
 
+    browserify: {
+      debug: true,
+      transform: [ 'babelify' ]
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
