d8aaadba38bfcf7f205bb125cd3325124c84ff5a
Matthew Hill <matthew.jh@outlook.com>
2015-09-28 20:43:24 +0100

    chore(test): adds es6-promise for tests

--- karma.conf.js
+++ karma.conf.js
@@ -65,6 +65,10 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: true,
+
+    onPrepare: function () {
+      require('es6-promise');
+    }
   })
 }
