be3322de10bcb96218c564516597bff61df081ac
Matthew Hill <matthew.jh@outlook.com>
2015-10-13 19:19:43 +0100

    0.1.0

---
+++ dist/jasmine-promise.js
@@ -0,0 +1,87 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+'use strict';
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
+
+var _patch = require('./patch');
+
+var patch = _interopRequireWildcard(_patch);
+
+if (!global.jasmine) {
+  throw new Error('jasmine must be loaded before jasmine-promise');
+}
+
+patch.apply();
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./patch":2}],2:[function(require,module,exports){
+(function (global){
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.apply = apply;
+function patchFunction(obj, slot, fnArgIndex) {
+  var delegate = obj[slot];
+
+  obj[slot] = function () {
+    var testFn = arguments[fnArgIndex];
+
+    arguments[fnArgIndex] = function (done) {
+      var testFnHasDoneArg = testFn.length >= 1;
+      var returnValue = undefined;
+
+      if (testFnHasDoneArg) {
+        returnValue = testFn(done);
+      } else {
+        returnValue = testFn();
+        if (returnValue && returnValue.then) {
+          returnValue.then(function () {
+            done();
+          });
+
+          if (returnValue['catch'] && done.fail) {
+            returnValue['catch'](function (err) {
+              done.fail(err);
+            });
+          }
+        } else {
+          done();
+        }
+      }
+
+      return returnValue;
+    };
+
+    return delegate.apply(this, arguments);
+  };
+}
+
+function patchInterfaceObj(interfaceObj) {
+  var targets = [{ slot: 'afterEach', fnArgIndex: 0 }, { slot: 'beforeEach', fnArgIndex: 0 }, { slot: 'beforeAll', fnArgIndex: 0 }, { slot: 'afterAll', fnArgIndex: 0 }, { slot: 'it', fnArgIndex: 1 }, { slot: 'fit', fnArgIndex: 1 }];
+
+  targets.forEach(function (target) {
+    patchFunction(interfaceObj, target.slot, target.fnArgIndex);
+  });
+}
+
+function patchInterfaceFn(obj) {
+  var delegate = obj['interface'];
+  obj['interface'] = function () {
+    var interfaceObj = delegate.apply(this, arguments);
+
+    patchInterfaceObj(interfaceObj);
+
+    return interfaceObj;
+  };
+}
+
+function apply() {
+  patchInterfaceObj(global);
+  patchInterfaceFn(global.jasmineRequire);
+}
+
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1]);
