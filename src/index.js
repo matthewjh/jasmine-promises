9b62db089ba3c4651656965355d94bdf596e9975
Matthew Hill <matthew.jh@outlook.com>
2016-01-30 22:37:59 +0000

    refactor(src/index): changes patch apply import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import * as patch from './patch';
+import {apply} from './patch';
 
 if (!global.jasmine) {
   throw new Error('jasmine must be loaded before jasmine-promise');
 }
 
-patch.apply();
+apply();
